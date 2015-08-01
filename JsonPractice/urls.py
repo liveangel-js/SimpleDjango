37ca034966cfb028596129e6c7ee500bfa816ace
liveangel <liveangel.js@gmail.com>
2015-08-01 22:11:26 +0800

    渲染json到模板

--- JsonPractice/urls.py
+++ JsonPractice/urls.py
@@ -5,6 +5,10 @@ __project__ = 'SimpleDjango'
 from django.conf.urls import *
 from JsonPractice.views import *
 
+"""
+该练习是输出静态输出json到js上
+"""
+
 urlpatterns = patterns('',
                        url(r'^$', home),
                        url(r'^json$', jsontest),
