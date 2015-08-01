5940f9f3562157fada6acbdd54f56e51f953064b
liveangel <liveangel.js@gmail.com>
2015-08-01 19:28:34 +0800

    给book增加缓存

--- book/views.py
+++ book/views.py
@@ -6,11 +6,29 @@ from django.template import loader, Context
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from book.models import *
+from django.core.cache import cache
 
 def booklist(request):
-    books = Book.objects.all()
-    publishers = Publisher.objects.all()
-    authors = Author.objects.all()
+    if cache.get('book'):
+        books = cache.get('book')
+    else:
+        books = Book.objects.all()
+        cache.set('book', books)
+
+    if cache.get('publishers'):
+        publishers = cache.get('publisher')
+    else:
+        publishers = Publisher.objects.all()
+        cache.set('publishers', publishers)
+
+    if cache.get('authors'):
+        authors = cache.get('authors')
+    else:
+        authors = Author.objects.all()
+        cache.set('authors', authors)
+
+
+
     #software engineer 的数量
     b_count = Book.objects.get_book_count('software engineer')
 
